78847d6ead09f9a6fec9d4d311e21785b8fda424
XJWM <2970285133@qq.com>
2025-02-19 00:48:41 +0800

    Update srun_md5.py

--- encryption/srun_md5.py
+++ encryption/srun_md5.py
@@ -3,6 +3,6 @@ import hashlib
 def get_md5(password,token):
 	return hmac.new(token.encode(), password.encode(), hashlib.md5).hexdigest()
 if __name__ == '__main__':
-	password="275059"
+	password=""#Your Password
 	token="711ab370231392679fe06523b119a8fe096f5ed9bd206b4de8d7b5b994bbc3e5"
 	print(get_md5(password,token))
